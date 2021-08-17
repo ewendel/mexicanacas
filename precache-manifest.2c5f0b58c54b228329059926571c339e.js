6ed8822cf49f77ecfae499b2a7354cc2eb657c3a
Erik Wendel <erik.wendel@vipps.no>
2021-08-17 10:19:56 +0200

    Kopiere over buildfolder fra react-app

---
+++ precache-manifest.2c5f0b58c54b228329059926571c339e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4539a44841626897069d9308b4cb576e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9a4cc29b8b3feb1b40f",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "bdfb451b9e7ceed585f5",
+    "url": "/static/css/main.5eeab774.chunk.css"
+  },
+  {
+    "revision": "c9a4cc29b8b3feb1b40f",
+    "url": "/static/js/2.b164d10e.chunk.js"
+  },
+  {
+    "revision": "cae1b573efd37b7d89c6086b35f3367f",
+    "url": "/static/js/2.b164d10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfb451b9e7ceed585f5",
+    "url": "/static/js/main.e8515256.chunk.js"
+  },
+  {
+    "revision": "3f807c72d45e8a0d3273",
+    "url": "/static/js/runtime-main.fb82294c.js"
+  },
+  {
+    "revision": "bb00ec5b39a6b3c0378883029adf0e9a",
+    "url": "/static/media/LosMex.bb00ec5b.svg"
+  },
+  {
+    "revision": "8cf7a6261b9a7a52dfdf2d10c21983ae",
+    "url": "/static/media/LosProv.8cf7a626.svg"
+  }
+]);
